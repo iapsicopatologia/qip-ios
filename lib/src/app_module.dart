b2ba9555981a94c9c480eff5b053a72f46908ded
josuemoraisgh <josuemorais@gmail.com>
2024-04-17 13:40:39 -0300

    refactor navegation

--- lib/src/app_module.dart
+++ lib/src/app_module.dart
@@ -5,7 +5,7 @@ import 'modules/home/telas_module.dart';
 class AppModule extends Module {
   @override
   void routes(r) {
-    r.module('/qip/', module: TelasModule());      
+    r.module('/qip-ios/', module: TelasModule());      
     r.module('/', module: TelasModule());        
     r.wildcard(child: (_) => const NotFoundPage());
   }
